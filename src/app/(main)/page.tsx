a26828835d03b370b112f75586610f2cbc53e8ea
Helen Lee Kupp <139151051+helenlkupp@users.noreply.github.com>
2025-04-06 11:29:48 -0700

    up

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,16 +1,16 @@
 'use client';
 
-import { StoryInputForm } from "@/components/form/StoryInputForm";
-import { StoryContainer } from "@/components/story/StoryContainer";
-import { useStoryStore } from "@/store/storyStore";
-import React from "react";
+import { StoryInputForm } from '@/components/form/StoryInputForm';
+import { StoryContainer } from '@/components/story/StoryContainer';
+import { useStoryStore } from '@/store/storyStore';
+import React from 'react';
 
 export default function HomePage() {
-  const generatedStory = useStoryStore((state) => state.generatedStory);
+	const generatedStory = useStoryStore((state) => state.generatedStory);
 
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4">
-        {!generatedStory ? <StoryInputForm /> : <StoryContainer />}
-    </div>
-  );
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen p-4">
+			{!generatedStory ? <StoryInputForm /> : <StoryContainer />}
+		</div>
+	);
 }
